8e03ecac0beceebb338c503f2b25c7f037bb385c
Trimbkeshwar Jagtap <jagtap.t@northeastern.edu>
2025-02-19 13:49:40 -0500

    l

--- app/routes/health_check.py
+++ app/routes/health_check.py
@@ -16,7 +16,7 @@ def health_check():
         
         response = make_response("", 200)
     else:
-        response = make_response("", 503)  # Service Unavailable
+        response = make_response("", 504)  # Service Unavailable
 
     # Add headers
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate;"
